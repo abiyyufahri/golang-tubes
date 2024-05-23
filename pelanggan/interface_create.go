77872523d5ababfc729bfdc40763f7db44e187d5
Afriza Gilleon Ginting <afriza.gilleon@gmail.com>
2024-05-23 20:17:18 +0700

    Interface untuk update pelanggan

--- pelanggan/interface_create.go
+++ pelanggan/interface_create.go
@@ -58,3 +58,77 @@ func create_form(p *Pelanggan) {
 		fmt.Println("error in pelanggan's create form:", err)
 	}
 }
+
+func update_form(p *Pelanggan) {
+
+	var nama, alamat, nomorTelepon, alamatEmail string
+	var confirm bool
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Nama").
+				Description("Masukkan nama pelanggan Anda").
+				Placeholder(p.nama).
+				Value(&nama),
+
+			huh.NewInput().
+				Title("Alamat").
+				Description("Tempat pengiriman ke pelanggan").
+				Placeholder(p.alamat).
+				Value(&alamat),
+
+			huh.NewInput().
+				Title("No. Hp").
+				Description("Nomor telp/hp yang dapat dihubungi").
+				Placeholder(p.nomorTelepon).
+				Value(&nomorTelepon),
+
+			huh.NewInput().
+				Title("Email").
+				Description("Email untuk informasikan ke pengguna").
+				Placeholder(p.alamatEmail).
+				Value(&alamatEmail),
+		),
+
+		huh.NewGroup(
+			//huh.NewSelect[bool]().
+			//	Title("Status").
+			//	Description("Bila nonaktif, pengguna tidak bisa dibuatkan ekspedisi").
+			//	Options(
+			//		huh.NewOption[bool]("Aktif", true),
+			//		huh.NewOption[bool]("Nonaktif", false),
+			//	).
+			//	Value(&p.status),
+
+			huh.NewConfirm().
+				Title("Konfirmasi?").
+				Description("Perubahan yang terjadi tidak dapat diulang").
+				Value(&confirm).
+				Affirmative("Yes!").
+				Negative("No."),
+		),
+	)
+
+	if confirm {
+		if nama != "" {
+			p.nama = nama
+		}
+
+		if alamat != "" {
+			p.alamat = alamat
+		}
+
+		if alamatEmail != "" {
+			p.alamatEmail = alamatEmail
+		}
+
+		if nomorTelepon != "" {
+			p.nomorTelepon = nomorTelepon
+		}
+	}
+
+	err := form.Run()
+	if err != nil {
+		fmt.Println("error in pelanggan's create form:", err)
+	}
+}
